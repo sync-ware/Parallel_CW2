9352f1b58cf33b463b00d5a6d9e6fb35644f7b05
sync-ware <saahilms@gmail.com>
2021-12-23 16:14:54 +0000

    Change of approach, will use scatter and gather

--- main.c
+++ main.c
@@ -2,32 +2,63 @@
 #include <mpi.h>
 #include <stdlib.h>
 
-double* make_matrix(int size, double init_value){
-	double* matrix = malloc(sizeof(double)*(size*size));
-	for(int x = 0; x < size*size; x++){
-		if (x < size || x % size == 0){
-			matrix[x] = init_value;
+double** make_matrix(int size, double init_value){
+	double** matrix = malloc(sizeof(double*)*size);
+	for(int y = 0; y < size; y++){
+		matrix[y] = malloc(sizeof(double)*size);
+		for (int x = 0; x < size; x++){
+			if (y == 0 || x == 0){
+				matrix[y][x] = init_value;
+			}
 		}
 	}
 	return matrix;
 }
 
-void print_matrix(double* matrix, int size){
-	for(int x = 0; x < size; x++){
-		for(int y = 0; y < size; y++){
-			printf("%f ", matrix[x*size + y]);
+void print_matrix(double** matrix, int size){
+	for(int y = 0; y < size; y++){
+		for(int x = 0; x < size; x++){
+			printf("%f ", matrix[y][x]);
 		}
 		printf("\n");
 	}
 }
 
+void print_array(double* array, int size){
+	for(int x = 0; x < size; x++){
+		printf("%f, ", array[x]);
+		if ((x+1) % 5 == 0){
+			printf("\n");
+		}
+	}
+	printf("\n");
+}
+
+double* convert_matrix(double** matrix, int size){
+	double* p_array = malloc(sizeof(double)*((size-2)*(size-2))*5);
+	int i = 0;
+	for(int y = 1; y < size-1; y++){
+		for (int x = 1; x < size-1; x++){
+			p_array[i] = matrix[y][x-1]; // Left
+			p_array[++i] = matrix[y-1][x]; // Top
+			p_array[++i] = matrix[y][x+1]; // Right
+			p_array[++i] = matrix[y+1][x]; // Bottom
+			p_array[++i] = matrix[y][x]; // Centre
+		}
+	}
+
+	return p_array;
+}
+
 int main(int argc, char** argv){
 	int rc, myrank, nproc, namelen;
 
-	int matrix_size = 15;
-	double* matrix = make_matrix(matrix_size, 1.0);
+	int matrix_size = 5;
+	double** matrix = make_matrix(matrix_size, 1.0);
 	print_matrix(matrix, matrix_size);
-
+	printf("\n");
+	double* p_array = convert_matrix(matrix, matrix_size);
+	print_array(p_array, ((matrix_size-2)*(matrix_size-2))*5);
 	char name[MPI_MAX_PROCESSOR_NAME];
 
 	rc = MPI_Init(&argc, &argv);
@@ -41,23 +72,23 @@ int main(int argc, char** argv){
 
 	if (myrank == 0){
 		printf("main reports %d procs\n", nproc);
-
+		
 	}
 
-	int p_size = ((matrix_size-1)*(matrix_size-1))/nproc;
-	int rem = ((matrix_size-1)*(matrix_size-1))%nproc;
-	double* p_matrix = malloc(sizeof(double)*p_size);
-	int start_point = (myrank*p_size)+matrix_size+1;
-	for(int x = start_point; x < p_size+start_point; x++){
+	// int p_size = ((matrix_size-1)*(matrix_size-1))/nproc;
+	// int rem = ((matrix_size-1)*(matrix_size-1))%nproc;
+	// double* p_matrix = malloc(sizeof(double)*p_size);
+	// int start_point = (myrank*p_size)+matrix_size+1;
+	// for(int x = start_point; x < p_size+start_point; x++){
 
-		double left_value = matrix[x-1];
-		double right_value = matrix[x-1];
-		double top_value = matrix[x-matrix_size];
-		double bottom_value = matrix[x+matrix_size];
-		double avg = (left_value+right_value+top_value+bottom_value)/4.0;
-		p_matrix[x-start_point] = avg;
+	// 	double left_value = matrix[x-1];
+	// 	double right_value = matrix[x-1];
+	// 	double top_value = matrix[x-matrix_size];
+	// 	double bottom_value = matrix[x+matrix_size];
+	// 	double avg = (left_value+right_value+top_value+bottom_value)/4.0;
+	// 	p_matrix[x-start_point] = avg;
 
-	}
+	// }
 
 	namelen = MPI_MAX_PROCESSOR_NAME;
 	MPI_Get_processor_name(name, &namelen);
